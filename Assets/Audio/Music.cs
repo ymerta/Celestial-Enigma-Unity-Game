5e4f51e3ceb9179986e088d266ac3aa99cd235a0
Dilara Yargıcı <dilarayargici08@outlook.com>
2025-05-29 13:17:03 +0300

    Audio

---
+++ Assets/Audio/Music.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Music : MonoBehaviour
+{
+    private static Music instance;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
